3ee6cbf893fb035318496112bca21e1da7748be1
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-07-07 00:09:43 -0500

    Added puppers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function capitalize(s) {
+  return s && s[0].toUpperCase() + s.slice(1);
+}
 
 function App() {
+  const [breeds, setBreeds] = useState({});
+  const [breed, setBreed] = useState("");
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    async function getBreeds() {
+      const response = await fetch("https://dog.ceo/api/breeds/list/all");
+      const data = await response.json();
+      setBreeds(data.message);
+    }
+    getBreeds().then();
+  }, []);
+
+  useEffect(() => {
+    const getRandom = async () => {
+      const response = await fetch(
+        "https://dog.ceo/api/breeds/image/random/10"
+      );
+      const data = await response.json();
+      setPhotos(data.message);
+    };
+    getRandom().then();
+  }, []);
+
+  useEffect(() => {
+    async function fetchBreed() {
+      const response = await fetch(
+        "https://dog.ceo/api/breed/" + breed + "/images/random/10"
+      );
+      const data = await response.json();
+      setPhotos(data.message);
+    }
+    if (breed) {
+      fetchBreed().then();
+    }
+  }, [breed]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Find a dog</h1>
+      <select
+        name="breed"
+        id="breed"
+        value={breed}
+        onChange={(e) => setBreed(e.target.value)}
+      >
+        <option value="">Select breed</option>
+        {Object.entries(breeds).map(([breed]) => (
+          <option value={breed} key={breed}>
+            {capitalize(breed)}
+          </option>
+        ))}
+      </select>
+      <div className="gallery">
+        {photos.map((photo) => (
+          <div className="photo" key={photo}>
+            <img src={photo} alt={"dog"} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
